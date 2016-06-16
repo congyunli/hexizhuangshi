b000f11f236d18dd07518fc190dd1c82943686a8
admin <admin@admindeMac-mini-10.local>
2016-06-16 19:36:54 +0800

    fix

--- caches/caches_commons/caches_data/common.cache.php
+++ caches/caches_commons/caches_data/common.cache.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  'admin_phone' => '18612117805',
-  'admin_contactEmail' => '398953597@qq.com',
+  'admin_phone' => '0411-84586967',
+  'admin_contactEmail' => '123@qq.com',
   'admin_email' => 'xxxxx@qq.com',
-  'admin_companyName' => '青岛傲雪世界数码科技有限公司',
-  'admin_address' => '山东省青岛市市北区都昌路3号',
-  'admin_jd' => '120.372874',
-  'admin_wd' => '36.122282',
+  'admin_companyName' => '大连和玺装饰装修工程有限公司',
+  'admin_address' => '大连市沙河口区五一路解放广场幸福家居临街16号',
+  'admin_jd' => '0',
+  'admin_wd' => '0',
   'maxloginfailedtimes' => '8',
   'minrefreshtime' => '2',
   'mail_type' => '1',
--- caches/caches_commons/caches_data/sitelist.cache.php
+++ caches/caches_commons/caches_data/sitelist.cache.php
@@ -5,10 +5,10 @@ return array (
     'siteid' => '1',
     'name' => '默认站点',
     'dirname' => '',
-    'domain' => 'http://www.cgassist.com/',
-    'site_title' => '青岛傲雪数码科技有限公司',
-    'keywords' => '青岛傲雪数码科技有限公司,青岛傲雪,傲雪,CG,青岛傲雪世界',
-    'description' => '青岛傲雪数码科技有限公司是专业为游戏以及影视动画公司提供优秀的CG产品以及各种解决方案， 满足游戏和影视公司高质量制作需求的新兴企业。',
+    'domain' => 'http://www.dlhexi.com/',
+    'site_title' => '大连和玺装饰装修工程有限公司',
+    'keywords' => '大连和玺装饰装修工程有限公司',
+    'description' => '大连和玺装饰设计工程有限公司主要经营：等产品。公司尊崇“踏实、拼搏、责任”的企业精神，并以诚信、共赢、开创经营理念，创造良好的企业环境，以全新的管理模式，完善的技术，周到的服务，卓越的品质为生存根本，我们始终坚持用户至上 用心服务于客户，坚持用自己的服务去打动客户。',
     'release_point' => '',
     'default_style' => 'allsure',
     'template' => 'allsure',
@@ -24,7 +24,7 @@ return array (
   \'watermark_pos\' => \'9\',
 )',
     'uuid' => '5e2f7244-740d-11e5-951f-f0def148bce4',
-    'url' => 'http://www.cgassist.com/',
+    'url' => 'http://www.dlhexi.com/',
   ),
 );
 ?>
